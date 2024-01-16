a6a93348c69ce6e808d0a1fbd48998c92cae3646
Dhohir Pradana <dhohirkudus@gmail.com>
2024-01-16 14:15:38 +0700

    update app.py and pb_user/__init__.py

--- app.py
+++ app.py
@@ -8,21 +8,26 @@ from pb_user import list_user as list_user_handler
 
 app = Flask(__name__)
 
+
 @app.route('/users', methods=['GET'])
 def list_user():
     return list_user_handler()
+
 
 @app.route('/user/<user_id>', methods=['GET'])
 def detail_user(user_id):
     return detail_user_handler(user_id)
 
+
 @app.route('/user', methods=['POST'])
 def create_user():
     return create_user_handler()
 
+
 @app.route('/user/<user_id>', methods=['PATCH'])
 def update_user(user_id):
     return update_user_handler(user_id)
+
 
 @app.route('/user/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
--- pb_user/__init__.py
+++ pb_user/__init__.py
@@ -7,16 +7,19 @@ dotenv.load_dotenv()
 
 PB_URL = os.environ.get("PB_URL")
 
+
 def list_user():
     try:
         body = request.get_json()
-        res = requests.get(f'{PB_URL}/api/collections/tes_bda/records', json=body, timeout=10)
+        res = requests.get(
+            f'{PB_URL}/api/collections/tes_bda/records', json=body, timeout=10)
         data = res.json()
         status_code = res.status_code
 
         return jsonify(data), status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
+
 
 def detail_user(user_id):
     try:
@@ -31,16 +34,19 @@ def detail_user(user_id):
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
+
 def create_user():
     try:
         body = request.get_json()
-        res = requests.post('{PB_URL}/api/collections/tes_bda/records', json=body, timeout=10)
+        res = requests.post(
+            '{PB_URL}/api/collections/tes_bda/records', json=body, timeout=10)
         data = res.json()
         status_code = res.status_code
 
         return jsonify(data), status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
+
 
 def update_user(user_id):
     try:
@@ -58,13 +64,14 @@ def update_user(user_id):
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
+
 def delete_user(user_id):
     try:
-        res = requests.delete(f'{PB_URL}/api/collections/tes_bda/records/{user_id}', timeout=10)
+        res = requests.delete(
+            f'{PB_URL}/api/collections/tes_bda/records/{user_id}', timeout=10)
         data = res.json()
         status_code = res.status_code
 
         return jsonify(data), status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
-    
